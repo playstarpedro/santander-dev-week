152f06a8b86e6f84e0a62e6b274723c250d5e0c9
pedroDev <playstarpedro@gmail.com>
2025-04-16 13:34:47 -0300

    Exposing REST endpoints & Swagger config

---
+++ src/main/java/me/dio/controller/UserRestController.java
@@ -0,0 +1,36 @@
+package me.dio.controller;
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // TODO: Create UserDTO
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        User userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
